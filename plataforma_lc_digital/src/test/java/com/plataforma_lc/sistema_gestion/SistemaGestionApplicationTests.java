4a5038d0184a172aefe2ea131c6f0afb8242a6b2
olovj222 <ol.juengt@duocuc.cl>
2026-03-31 20:36:23 -0300

    estructura inicial

---
+++ plataforma_lc_digital/src/test/java/com/plataforma_lc/sistema_gestion/SistemaGestionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.plataforma_lc.sistema_gestion;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemaGestionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
