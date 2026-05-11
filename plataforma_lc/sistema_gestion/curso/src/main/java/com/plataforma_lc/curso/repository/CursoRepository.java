9b1a905fd2761c5d01b300c26c12e10c4833f500
olovj222 <ol.juengt@duocuc.cl>
2026-05-11 14:18:43 -0400

    desarrollo modulo cursos, métodos funcionando en conjunto con Estudiante

--- plataforma_lc/sistema_gestion/curso/src/main/java/com/plataforma_lc/curso/repository/CursoRepository.java
+++ plataforma_lc/sistema_gestion/curso/src/main/java/com/plataforma_lc/curso/repository/CursoRepository.java
@@ -2,9 +2,9 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/springframework/Repository.java to edit this template
  */
-package com.plataforma_lc.repository;
+package com.plataforma_lc.curso.repository;
 
-import com.plataforma_lc.entities.Curso;
+import com.plataforma_lc.curso.entities.Curso;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
