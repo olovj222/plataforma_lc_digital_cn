7beb5ca4d396cbdd97366335f05c20c17cc50de5
olovj222 <ol.juengt@duocuc.cl>
2026-05-16 21:53:01 -0400

    manejo excepciones y schema para cursos

--- plataforma_lc/sistema_gestion/curso/src/main/java/com/plataforma_lc/curso/entities/Curso.java
+++ plataforma_lc/sistema_gestion/curso/src/main/java/com/plataforma_lc/curso/entities/Curso.java
@@ -4,6 +4,7 @@
  */
 package com.plataforma_lc.curso.entities;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.persistence.*;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
@@ -13,19 +14,23 @@ import lombok.Data;
 @Entity
 public class Curso {
 
+    @Schema(accessMode = Schema.AccessMode.READ_ONLY, example = "1")
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @Schema(example = "Matemáticas")
     @NotBlank(message = "El nombre del curso es obligatorio")
-    private String nombre; // Ej: "1° Básico A"
+    private String nombre;
 
+    @Schema(example = "101")
     @NotNull(message = "El Codigo es obligatorio")
     private Integer codigo;
 
-    // ID del profesor jefe (referencia al MS de usuarios/profesores, no FK real)
+    @Schema(example = "1")
     private Long profesorId;
 
+    @Schema(hidden = true)
     @Transient
-    private String profesorNombre; // Se llena con WebClient si hay MS de usuarios
+    private String profesorNombre;
 }
