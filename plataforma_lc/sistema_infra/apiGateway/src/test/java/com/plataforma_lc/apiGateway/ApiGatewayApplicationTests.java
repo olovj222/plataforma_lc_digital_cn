290645e848769d0a989345e554435e77f5394c81
olovj222 <ol.juengt@duocuc.cl>
2026-09-09 19:04:18 -0300

    modificaciones apigw sin keycloak

--- plataforma_lc/sistema_infra/apiGateway/src/test/java/com/plataforma_lc/apiGateway/ApiGatewayApplicationTests.java
+++ plataforma_lc/sistema_infra/apiGateway/src/test/java/com/plataforma_lc/apiGateway/ApiGatewayApplicationTests.java
@@ -6,7 +6,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest(
     properties = {
         "eureka.client.enabled=false",
-        "spring.security.oauth2.resourceserver.jwt.issuer-uri=http://localhost:8090/realms/plataforma-lc"
+        "spring.security.oauth2.resourceserver.jwt.issuer-uri=https://login.microsoftonline.com/2dcf78c8-4359-4115-8b06-50c5a455e4e0/v2.0"
     },
     webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT
 )
