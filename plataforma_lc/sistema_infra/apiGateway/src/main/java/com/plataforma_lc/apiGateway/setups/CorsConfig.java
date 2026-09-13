eaacd6b5b96f1933ceb88cf5830058bc9d50a39d
Alex9-hub <axelhead9@gmail.com>
2026-09-13 15:22:59 -0300

    cloudfare

--- plataforma_lc/sistema_infra/apiGateway/src/main/java/com/plataforma_lc/apiGateway/setups/CorsConfig.java
+++ plataforma_lc/sistema_infra/apiGateway/src/main/java/com/plataforma_lc/apiGateway/setups/CorsConfig.java
@@ -1,7 +1,4 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package com.plataforma_lc.apiGateway.setups;
 
 import org.springframework.context.annotation.Bean;
@@ -16,9 +13,11 @@ public class CorsConfig {
     @Bean
     public CorsWebFilter corsWebFilter() {
         CorsConfiguration config = new CorsConfiguration();
+        // Solo permite entorno local y cualquier túnel generado por Cloudflare
         config.addAllowedOrigin("http://localhost:5173");
-        config.addAllowedMethod("*");
-        config.addAllowedHeader("*");
+        config.addAllowedOriginPattern("https://*.trycloudflare.com");
+        config.addAllowedMethod("");
+        config.addAllowedHeader("");
         config.setAllowCredentials(true);
 
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
